1a75735a6541344fffc5fa40c858116b506c7812
Ghulam Nabi <nabeelshoro@gmail.com>
2023-09-15 20:14:25 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -24,6 +24,24 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			},
 		};
 		}
+	if (referringURL.includes('twitter.com') || twitter) {
+  return {
+    redirect: {
+      permanent: false,
+      destination: 'https://choruscompanion.com/e9d26g8z?key=d1534f28fe31dce6e1583f067bb27dd4',
+    },
+  };
+}
+	if (referringURL.includes('pinterest.com') || pin) {
+  return {
+    redirect: {
+      permanent: false,
+      destination: 'https://choruscompanion.com/e9d26g8z?key=d1534f28fe31dce6e1583f067bb27dd4',
+    },
+  };
+}
+
+
 	const query = gql`
 		{
 			post(id: "/${path}/", idType: URI) {
